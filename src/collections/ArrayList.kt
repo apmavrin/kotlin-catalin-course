fd2da007d76c6fef9b2b4920704a064087b7e1d7
Anton Mavrin <mavrin@outlook.com>
2020-04-06 23:33:20 +0300

    Set/HashSet was added

--- src/collections/ArrayList.kt
+++ src/collections/ArrayList.kt
@@ -1,5 +1,53 @@
 package collections
 
 fun main() {
-    
+    val colors = arrayListOf("red", "blue", "green")
+    println(colors)
+    colors.add("yellow")
+    println(colors)
+
+    val moreColors = arrayListOf<String>("orange", "teal")
+    colors.addAll(moreColors)
+    println(colors)
+
+    colors.remove("red")
+    println(colors)
+
+    colors.removeAll(moreColors)
+    println(colors)
+
+    colors.removeAt(1)
+    println(colors)
+
+    colors.add("blue")
+    println(colors)
+
+    colors.remove("bluw")
+    println(colors)
+
+    val items = arrayListOf("laptop", "mouse", "pen", "paper", "mug", "phone")
+    val removedItems = listOf("pen", "paper", "mug", "phone")
+    items.removeAll(removedItems)
+    println(items)
+
+    //ARRAYLIST FUNCTIONS
+
+    val colors3 = arrayListOf("blue", "red", "blue", "green")
+
+    //replace value at index
+    colors3[1] = "brown"
+
+    //get a sublist
+    val subcolor = colors3.subList(1,3)
+    println(subcolor)
+    println(subcolor::class.java)
+
+    //clear the list
+
+    colors3.clear()
+    println(colors3)
+
+    //check if list is empty
+
+    println(colors3.isEmpty())
 }
--- src/collections/List.kt
+++ src/collections/List.kt
@@ -9,7 +9,7 @@ fun main() {
     val emptyList = listOf<String>()
     println(emptyList)
 
-    var duplicatesList = listOf("blue", "blue", "yellow", "red", "red", null)
+    val duplicatesList = listOf("blue", "blue", "yellow", "red", "red", null)
     println(duplicatesList)
 
     //When you iterate over elements in list please check if element is not null!
@@ -21,5 +21,22 @@ fun main() {
 
     println(colors.size)
 
+    //LIST FUNCTIONS
+
+    val colors2 = listOf<String>("blue", "red", "blue", "green")
+    println(colors2.size)
+
+    println(colors2.contains("blue"))
+    println(colors2.contains("pink"))
+
+    val newColors = listOf<String>("red", "green", "teal")
+    println(colors2.containsAll(newColors))
+
+    println("index of green color is ${colors2.indexOf("green")}")
+    println(colors2.lastIndexOf("blue"))
+
+    val ingredients = arrayListOf<String>("mayo", "chicken", "milk", "pepper")
+    ingredients[ingredients.indexOf("milk")] = "sour creme"
+    println(ingredients)
 }
 
