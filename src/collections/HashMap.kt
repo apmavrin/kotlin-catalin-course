087497ca1b94fd418084a0aca4924715ab9008a6
Anton Mavrin <mavrin@outlook.com>
2020-04-09 22:11:14 +0300

    flowControl added

--- src/collections/HashMap.kt
+++ src/collections/HashMap.kt
@@ -21,4 +21,22 @@ fun main() {
     count.clear()
     println(count)
 
+    //HashMap functions
+    println("-------HashMap functions--------\n")
+    val count2 = hashMapOf(Pair(1, "one"), Pair(2,"two"), Pair(3, "three"))
+
+    println("This hashmap has a size of ${count2.size}")
+    println("Does the map have key 2? ${count2.containsKey(2)}")
+    println("Does the map have key 5? ${count2.containsKey(5)}")
+    println("Does the map have value \"one\"? ${count2.containsValue("one")}")
+    println("Does the map have value \"five\"? ${count2.containsValue("five")}")
+    println("Is the map empty? ${count2.isEmpty()}")
+    println("Does the map have any kay value pairs? ${count2.isNotEmpty()}")
+    println(count2[1])
+
+    val dayArray = hashMapOf(Pair("one", 1), Pair("two", 2))
+
+    val firstOne = dayArray["one"]?:0
+    val secondOne = dayArray["two"]?:0
+    println(firstOne + secondOne)
 }
---
+++ src/flowControl/if_conditional.kt
@@ -0,0 +1,10 @@
+package flowControl
+
+/**
+ * if - only 2 options: true or false
+ * when - variable or expression: various options incl. default.
+**/
+
+fun main() {
+
+}
